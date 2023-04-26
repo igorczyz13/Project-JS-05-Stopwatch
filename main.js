3642594201d7997ff282982eb66f30aee082d251
Igor Czyż <igorczyz13@gmail.com>
2023-04-26 19:10:21 +0200

    Fix all bugs

--- main.js
+++ main.js
@@ -6,7 +6,8 @@ const historyBtn = document.querySelector('.history');
 const stopwatch = document.querySelector('.stopwatch');
 const time = document.querySelector('.time');
 const timeList = document.querySelector('.time-list');
-const infoBtn = document.querySelector('.info');
+
+const infoBtn = document.querySelector('.fa-question');
 const modalShadow = document.querySelector('.modal-shadow');
 const closeModalBtn = document.querySelector('.close');
 
@@ -16,8 +17,16 @@ let seconds = 0;
 
 let timesArr = [];
 
-const handleStart = () => {
+// zmiana kolorów
+const colorBtn = document.querySelector('.fa-paint-brush');
+const colorPanel = document.querySelector('.colors');
+const colorOne = document.querySelector('.one');
+const colorTwo = document.querySelector('.two');
+const colorThree = document.querySelector('.three');
+let root = document.documentElement;
 
+
+const handleStart = () => {
     clearInterval(countTime);
 
     countTime = setInterval(() => {
@@ -32,22 +41,23 @@ const handleStart = () => {
             seconds = 0;
             stopwatch.textContent = `${minutes}:00`
         }
-    }, 1000);
+    }, 100);
 }
 
 const handleStop = () => {
 
-    time.innerHTML = `Last time: ${stopwatch.textContent}`
+    time.innerHTML = `Ostatni czas: ${stopwatch.textContent}`
 
     if(stopwatch.textContent !== '0:00') {
         time.style.visibility = 'visible';
         timesArr.push(stopwatch.textContent)
-    }
+    };
+
     clearStuff();
 }
 
 const handlePause = () => {
-    clearInterval(countTime)
+    clearInterval(countTime);
 }
 
 const handleReset = () => {
@@ -65,21 +75,24 @@ const clearStuff = () => {
 }
 
 const showHistory = () => {
+
     timeList.textContent = '';
     let num = 1;
+
     timesArr.forEach(time => {
         const newTime = document.createElement('li');
-        newTime.innerHTML = `Measurement nr ${num}: <span>${time}</span>`
-        timeList.appendChild(newTime)
+        newTime.innerHTML = `Pomiar nr ${num}: <span>${time}</span>`
+
+        timeList.appendChild(newTime);
         num++;
     })
 }
 
 const showModal = () => {
-    if(!(modalShadow.style.display === 'block')){
-        modalShadow.style.display = 'block'
+    if (!(modalShadow.style.display === 'block')) {
+        modalShadow.style.display = 'block';
     } else {
-        modalShadow.style.display = 'none'
+        modalShadow.style.display = 'none';
     };
 
     modalShadow.classList.toggle('modal-animation')
@@ -90,6 +103,28 @@ pauseBtn.addEventListener('click', handlePause);
 stopBtn.addEventListener('click', handleStop);
 resetBtn.addEventListener('click', handleReset);
 historyBtn.addEventListener('click', showHistory);
+
 infoBtn.addEventListener('click', showModal);
 closeModalBtn.addEventListener('click', showModal);
 window.addEventListener('click', e => e.target === modalShadow ? showModal() : false);
+
+
+// zmiana kolorów
+colorBtn.addEventListener('click', () => {
+    colorPanel.classList.toggle('show-colors')
+})
+
+colorOne.addEventListener('click', () => {
+    root.style.setProperty('--first-color', 'rgb(250, 20, 6)');
+    root.style.setProperty('--hover-color', 'rgb(209, 33, 24)');
+});
+
+colorTwo.addEventListener('click', () => {
+    root.style.setProperty('--first-color', 'rgb(6, 173, 250)');
+    root.style.setProperty('--hover-color', 'rgb(28, 145, 199)');
+});
+
+colorThree.addEventListener('click', () => {
+    root.style.setProperty('--first-color', 'rgb(0, 255, 42)');
+    root.style.setProperty('--hover-color', 'rgb(28, 209, 58)');
+});
